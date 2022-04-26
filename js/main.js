b615adc8655842ced4769e71d3cac6835e279408
mrVoit <53047165+mrVoit@users.noreply.github.com>
2022-04-26 21:24:34 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ['Front-End developer', 'Web-designer'],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 5000,
+    loop: true
+  };
+  
+  var typed = new Typed('.type', options);
